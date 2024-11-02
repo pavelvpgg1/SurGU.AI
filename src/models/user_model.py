2b362597ce4f291b408dcaf286f4b3173a8556fe
Shelbyen <ars.sheberstov@mail.ru>
2024-11-03 03:10:18 +0500

    create models and migrations

---
+++ src/models/user_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base_model import Base
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[str] = mapped_column(String, primary_key=True)
+    is_man: Mapped[bool]
+    age: Mapped[int]
+    dialog: Mapped[str] = mapped_column(Text)
